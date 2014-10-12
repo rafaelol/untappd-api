aa4984612ddf1348bd79adedef26287f6ac56176
rafael <rafael@rafael.(none)>
2014-10-12 00:14:27 -0300

    Still working on those changes =)
    - Many subs created
    - Fixed some tests
    - Worked a bit on documentation
    
    Still have to work more on documentation and testing stuff.

--- lib/Untappd/API.pm
+++ lib/Untappd/API.pm
@@ -10,6 +10,37 @@ has 'client_secret', is => 'ro', required => 1;
 has 'access_token',  is => 'ro'; 
 has 'endpoint',      is => 'rwp', default => 'https://api.untappd.com/v4';
 
+# User Methods
+
+sub user_feed {
+    my ($self, $username, $max_id, $limit) = @_;
+    return $self->_request_get("/user/checkins/$username", {max_id => $max_id,
+                                                            limit  => $limit});
+}
+
+sub user_info {
+    my ($self, $username) = @_;
+    return $self->_request_get("/user/info/$username");
+}
+
+sub user_info_compact {
+    my ($self, $username) = @_;
+    return $self->_request_get("/user/info/$username", {compact => 'true' });
+}
+
+sub user_badges {
+    my ($self, $username, $offset) = @_;
+    return $self->_request_get("/user/badges/$username", { offset => $offset }); 
+}
+
+sub user_friends {
+    my ($self, $username, $offset, $limit) = @_;
+    return $self->_request_get("/user/badges/$username", { offset => $offset,
+                                                           limit => $limit });
+}
+
+# Beer Methods
+
 sub beer_info {
     my ($self, $bid) = @_;
     return $self->_request_get("/beer/info/$bid");
@@ -40,13 +71,18 @@ sub _beer_search {
 }
 
 sub _request_get {
-    my ( $self, $method, $params ) = @_;
+    my ( $self, $method, $params_tmp ) = @_;
 
-    $params->{client_id}     = $self->client_id;
-    $params->{client_secret} = $self->client_secret;
-    
+    $params_tmp->{client_id}     = $self->client_id;
+    $params_tmp->{client_secret} = $self->client_secret;
+    $params_tmp->{access_token}  = $self->access_token;
+
+    my %params = map { defined $params_tmp->{$_} 
+        ? ( $_ => $params_tmp->{$_} )
+        : () } keys %$params_tmp;
+
     my $uri = URI->new( $self->endpoint . $method );
-    $uri->query_form( $params );
+    $uri->query_form( %params );
     my $lwp = LWP::UserAgent->new;
     my $res = $lwp->get( $uri );
     return $res->decoded_content;
@@ -70,14 +106,62 @@ do it for free on L<https://untappd.com/api/register>.
 
 =head2 new
 
+=head2 user_feed
+
+=head2 user_info
+
+=head2 user_info_compact
+
+=head2 user_badges
+
+=head2 user_friends
+
+=head2 beer_info 
+
+=head2 beer_info_compact
+
+=head2 beer_search_by_name
+
+=head2 beer_search_by_count
+
+=head1 To-Do
+
+=head2 User Methods
+User Wish List
+User Distinct Beer
+
+=head2 Beer Methods
+Beer Feed
+Trending
+
+=head2 Venue/Brewery Methods
+All of them. =)
+
+=head2 Untappd Methods
+All of them. =)
+
+=head1 Acknowledgments
+
+First of all, I thank Ninkasi, the tutelary goddess of beer, and all the 
+sumerian people for their creation. =)
+
+Second, thank you Untappd for creating this service (and recording my
+drinking history). =)
+
+Last, but not least, this module would not exist without the support, help
+and motivation given by GARU. Thanks, bro. =) 
+
+=head1 If you're in Brazil...
+
+Specially if you're in Rio... Send me a message. Let's drink some beer
+together! =)
+
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2014 Rafael Oliveira Lopes C<< <rlopes at cpan.org> >>. All rights reserved.
 
 This module is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself. See L<perlartistic>.
-
-
 
 =head1 DISCLAIMER OF WARRANTY
 
@@ -101,6 +185,4 @@ RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
 FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
 SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
 SUCH DAMAGES.
-
-
 
